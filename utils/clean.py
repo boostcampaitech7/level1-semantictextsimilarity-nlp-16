d68dc309657aad21bfb285cfdcc0ddd4f93da00a
Sunwoo Yoo <sunsense9@naver.com>
2024-09-22 02:43:40 +0900

    fix: Modify remove_korean_stopwords path

--- utils/clean.py
+++ utils/clean.py
@@ -1,9 +1,9 @@
 import re
 
 import nltk
+from konlpy.tag import Okt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from konlpy.tag import Okt
 
 nltk.download("punkt_tab")
 nltk.download("punkt")
@@ -13,32 +13,32 @@ nltk.download("stopwords")
 def remove_punctuation_and_special_chars(text):
     return re.sub(r"[^a-z0-9가-힣\s]", "", text)
 
+
 def remove_english_stopwords(text):
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words("english"))
     word_tokens = word_tokenize(text)
     result = [word for word in word_tokens if word.lower() not in stop_words]
     return " ".join(result)
 
+
 def open_textfile(file_path):
-    f = open(file_path, 'r')
+    f = open(file_path, "r")
     data = f.read()
-    print(data)
     f.close()
     return data
 
+
 def remove_korean_stopwords(text):
-    okt = Okt() # https://konlpy.org/ko/latest/install/
+    okt = Okt()  # https://konlpy.org/ko/latest/install/
 
-    stop_words = open_textfile("./korean_stopwords.txt")
-    print(stop_words)
+    stop_words = open_textfile("./utils/korean_stopwords.txt")
 
-    stop_words = set(word_tokenize(text))
-    word_tokens = okt.morphs(text) # 형태소 단위로 분해
-    print(stop_words)
-    print(word_tokens)
+    stop_words = set(word_tokenize(stop_words))
+    word_tokens = okt.morphs(text)  # 형태소 단위로 분해
 
     result = [word for word in word_tokens if not word in stop_words]
     return " ".join(result)
+
 
 def clean_text(text, remove_english_stop=False, remove_korean_stop=False):
     text = text.lower()
@@ -50,15 +50,3 @@ def clean_text(text, remove_english_stop=False, remove_korean_stop=False):
     if remove_korean_stop:
         text = remove_korean_stopwords(text)
     return text
-
-# def clean_text(text, remove_stop=False, language="english"):
-#     text = text.lower()
-#     text = remove_punctuation_and_special_chars(text)
-
-#     if remove_stop:
-#         text = remove_stopwords(text, language)
-#     return text
-
-
-# def clean_texts(texts, remove_stop=False, language="english"):
-#     return [clean_text(text, remove_stop, language) for text in texts]
