a6938f845eb1b586cc5fd0216c0a8e7cc85e8907
kangjun205 <kangjun205@gamil.com>
2024-09-22 07:38:58 +0000

    feat: Add argument parser setting

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import argparse
 import collections
-import datetime
 
 import numpy as np
 import pandas as pd
@@ -11,27 +10,31 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
-import data_loader.data_loaders as module_data
-import model.loss as module_loss
-import model.metric as module_metric
-import model.model as module_arch
 from data_loader.data_loaders import TextDataLoader
 from model.model import STSModel
-from utils import prepare_device
 from utils.preprocessing import preprocessing
 from utils.tokenizer import get_tokenizer
 from utils.util import WandbCheckpointCallback, set_seed
 
-# from trainer import Trainer
 
+def main(args):
+    ## initialize wandb
+    run = wandb.init()
+    ## call configuration from wandb
+    config = wandb.config
 
-def main(config):
     ## parameters
-    MODEL_NAME = config["MODEL_NAME"]
     EPOCHS = config["EPOCHS"]
     BATCH_SIZE = config["BATCH_SIZE"]
     LEARNING_RATE = config["LEARNING_RATE"]
     MAX_LEN = config["MAX_LEN"]
+
+    PATH = args.path
+    MODEL_NAME = args.model_name
+    SEED = args.seed
+
+    ## seed setting
+    set_seed(SEED)
 
     ## data
     train = pd.read_csv("data/train.csv", dtype={'label': np.float32})
@@ -59,15 +62,15 @@ def main(config):
 
     checkpoint_callback = ModelCheckpoint(
         dirpath="saved",
-        filename=f'{MODEL_NAME}',
+        filename=f'{epoch:02d}-{int(val_pearson_corr*1000):04d}',
         save_top_k=3,
-        monitor="val_loss",
+        monitor="val_pearson_corr",
         mode="min",
     )
 
     wandb_checkpoint_callback = WandbCheckpointCallback(top_k=3)
 
-    run_name = f'{MODEL_NAME}_'
+    run_name = f'{MODEL_NAME}_{LEARNING_RATE}'
     wandb_logger = WandbLogger(name=run_name, project="Level1-STS")
 
     trainer = Trainer(
@@ -75,7 +78,11 @@ def main(config):
         devices=1,
         max_epochs=EPOCHS,
         val_check_interval=1,
-        callbacks=[early_stop_callback, checkpoint_callback, wandb_checkpoint_callback],
+        callbacks=[
+            early_stop_callback,
+            checkpoint_callback,
+            wandb_checkpoint_callback
+        ],
         logger = wandb_logger
     )
 
@@ -85,36 +92,25 @@ def main(config):
 if __name__ == "__main__":
     args = argparse.ArgumentParser(description="PyTorch Template")
     args.add_argument(
-        "-c",
-        "--config",
-        default=None,
+        "-p",
+        "--path",
+        default="./data",
         type=str,
-        help="config file path (default: None)",
+        help="config data path (default: ./data)",
     )
     args.add_argument(
-        "-r",
-        "--resume",
+        "-m",
+        "--model_name",
         default=None,
         type=str,
-        help="path to latest checkpoint (default: None)",
+        help="what models to call (default: None)",
     )
     args.add_argument(
-        "-d",
-        "--device",
-        default=None,
-        type=str,
-        help="indices of GPUs to enable (default: all)",
+        "-s",
+        "--seed",
+        default=12345,
+        type=int,
+        help="give seed number for experiment (default: 12345)"
     )
 
-    # custom cli options to modify configuration from default values given in json file.
-    CustomArgs = collections.namedtuple("CustomArgs", "flags type target")
-    options = [
-        CustomArgs(["--lr", "--learning_rate"], type=float, target="optimizer;args;lr"),
-        CustomArgs(
-            ["--bs", "--batch_size"], type=int, target="data_loader;args;batch_size"
-        ),
-    ]
-    SEED = 42
-    set_seed(SEED)
-
-    main(wandb.config)
+    main(args)
