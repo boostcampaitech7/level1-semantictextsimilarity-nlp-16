25dd78be744f6948ee51b85d239269ce17ac3e53
kangjun205 <kangjun205@gamil.com>
2024-09-22 11:45:16 +0000

    feat: Add module name printer

---
+++ utils/get_module_name.py
@@ -0,0 +1,20 @@
+from transformers import AutoModel
+import argparse
+
+def main(arg):
+    MODEL_NAME = arg.model_name
+    model = AutoModel.from_pretrained(MODEL_NAME)
+    for name, module in model.named_modules():
+        print(name)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model_name",
+        type=str,
+        help="Enter the name of a model that requires module names for the mode"
+    )
+    arg = parser.parse_args()
+
+    main(arg)
