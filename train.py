e2d98b915fbad19b85b65d7cac5e43332a266b6c
Sunwoo Yoo <sunsense9@naver.com>
2024-09-25 20:54:24 +0900

    feat: add 이상치 행 삭제

--- train.py
+++ train.py
@@ -36,12 +36,33 @@ def main():
     SEED = config["SEED"]
     set_seed(SEED)
 
-    ## load, preprocess data
-    preprocess = False  # 전처리 데이터 적용시 True로 변경
+    ## load data
     data_dir = config["DATA_DIR"]
     train_dir = os.path.join(data_dir, "train.csv")
     dev_dir = os.path.join(data_dir, "dev.csv")
+    train = pd.read_csv(train_dir, dtype={"label": np.float32})
+    dev = pd.read_csv(dev_dir, dtype={"label": np.float32})
 
+    ## 이상치 행 삭제
+    filtered_ids = [757, 847, 924, 925, 926, 938, 952, 975, 1031, 1066, 
+		1117, 1119, 1122, 1163, 1180, 1191, 1193, 1203, 1204, 1223, 1233,
+		1254, 1275, 1301, 1308, 1312, 1321, 1359, 1376, 1379, 1388, 1417,
+		1421, 1458, 1494, 1522, 1533, 1544, 1548, 1610, 1615, 1613, 1618,
+		1627, 1628, 1649, 1674, 1694, 1711, 1726, 1731, 1741, 1764, 1791,
+		1805, 1809, 1818, 1871, 1872, 1881, 1913, 1919, 1920, 1923, 1946,
+		1949, 1957, 1960, 1969, 1975, 1997, 2871, 2585, 3044, 3147, 4713,
+		4811, 4818, 4868, 4873, 4923, 4946, 4958, 5006, 5041, 5073, 5113,
+		5120, 5190, 5236, 5257, 5293, 5297, 5354, 5355, 5360, 5365, 5404,
+		5405, 5408, 5465, 5495, 5501, 5524, 5538, 5587, 5591, 5594, 5643,
+		5689, 5691, 5700, 5753, 5762, 5829, 5862, 5894, 5896, 5897, 5941,
+		5960, 5971, 5986, 6059, 6145, 6232, 6255, 6559, 6581, 6622, 6701,
+		6898, 6907, 7231, 7378, 7396, 8981, 9318, 9026, 9112, 9122, 8140,
+		8138]
+    train.drop(index=filtered_ids, inplace=True)
+    train.reset_index(drop=True, inplace=True)
+
+    ## 데이터 전처리
+    preprocess = False  # 전처리 데이터 적용시 True로 변경
     preprocessed_train_dir = os.path.join(data_dir, "preprocessed_train.csv")
     preprocessed_dev_dir = os.path.join(data_dir, "preprocessed_dev.csv")
     if preprocess == True:
@@ -52,8 +73,6 @@ def main():
             train = pd.read_csv(preprocessed_train_dir, dtype={"label": np.float32})
             dev = pd.read_csv(preprocessed_dev_dir, dtype={"label": np.float32})
         else:
-            train = pd.read_csv(train_dir, dtype={"label": np.float32})
-            dev = pd.read_csv(dev_dir, dtype={"label": np.float32})
             print("Preprocessing train data...")
             train = preprocess_data(train)
             print(f"Saving preprocessed train data to {preprocessed_train_dir}")
@@ -67,7 +86,7 @@ def main():
         train = pd.read_csv(train_dir, dtype={"label": np.float32})
         dev = pd.read_csv(dev_dir, dtype={"label": np.float32})
 
-    ## 데이터 증강, 전처리와 동시 적용하려면 증강만 된 데이터를 지우고 전처리를 True로 설정 후 적용
+    ## 데이터 증강과 전처리를 동시 적용할 경우, augmented 데이터 삭제 후 preprocess를 True로 설정 후 적용
     augment = False  # 증강 적용시 True로 설정
     augmented_train_dir = os.path.join(data_dir, "augmented_train.csv")
     augmented_dev_dir = os.path.join(data_dir, "augmented_dev.csv")
