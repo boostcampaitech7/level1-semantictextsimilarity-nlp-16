eef584af05a4a198e7732269956b23b4d017fe5b
Sunwoo Yoo <sunsense9@naver.com>
2024-09-26 13:00:53 +0900

    feat: add weighted voting module

---
+++ utils/weighted_voting.py
@@ -0,0 +1,43 @@
+import argparse
+import glob
+import os
+
+import numpy as np
+import pandas as pd
+
+
+def main(arg):
+    path = os.path.join(arg.data_dir, "submission")
+    all_files = glob.glob(os.path.join(path, "*.csv"))
+
+    # 각 submission 파일에서 'target' 열을 가져옴
+    preds = [pd.read_csv(file)[["target"]] for file in all_files]
+
+    # 가중치 설정 (예를 들어, 성능에 따라 가중치 부여)
+    weights = [0.3, 0.3, 0.15, 0.15, 0.1]
+
+    w = [0, 0, 0, 0, 0]
+    w_s = sum(w)
+    wei = [i / w_s for i in w]
+
+    # 가중치 앙상블
+    ensemble_preds = np.average(preds, axis=0, weights=wei)
+
+    # 결과 저장
+    submission = preds[0].copy()
+    submission["target"] = ensemble_preds
+    submission.to_csv("weighted_voting.csv", index=False)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "-d",
+        "--data_dir",
+        default=None,
+        type=str,
+        help="directory path for data (default: None)",
+    )
+
+    arg = args.parse_args()
+    main(arg)
