7d7c6d88f945babab54403b841ea16473e5a6d6e
kangjun205 <kangjun205@gamil.com>
2024-09-26 11:46:53 +0000

    docs: Add doc string to data_loaders.py

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -5,13 +5,27 @@ from .datasets import TestDataset, TrainDataset
 
 
 class TextDataLoader(L.LightningDataModule):
+    """_summary_
+    텍스트 데이터를 처리 및 로드
+
+    train, validation, predict 데이터셋을 관리하고,
+    분석의 각 단계에 맞는 데이터로더를 제공
+    Args:
+        tokenizer: 문장을 토큰화하는 토크나이저
+        max_len (int): 최대 시퀀스 길이
+        train_data (pd.DataFrame): 학습 데이터
+        val_data (pd.DataFrame): 검증 데이터
+        predict_data (pd.DataFrame): 추론 대상 데이터
+        truncation (bool, optional): 문장 절단 여부
+        batch_size (int): 배치 크기
+    """
     def __init__(
         self,
         tokenizer,
         max_len,
         train_data=None,
-        dev_data=None,
-        test_data=None,
+        val_data=None,
+        predict_data=None,
         truncation=True,
         batch_size=32,
     ):
@@ -19,8 +33,8 @@ class TextDataLoader(L.LightningDataModule):
         self.tokenizer = tokenizer
         self.max_len = max_len
         self.train_data = train_data
-        self.dev_data = dev_data
-        self.test_data = test_data
+        self.val_data = val_data
+        self.predict_data = predict_data
         self.truncation = truncation
         self.batch_size = batch_size
 
@@ -33,17 +47,17 @@ class TextDataLoader(L.LightningDataModule):
                 tokenizer=self.tokenizer,
                 max_len=self.max_len,
             )
-            self.dev_dataset = TrainDataset(
-                sentence_1=self.dev_data["sentence_1"],
-                sentence_2=self.dev_data["sentence_2"],
-                labels=self.dev_data["label"],
+            self.val_dataset = TrainDataset(
+                sentence_1=self.val_data["sentence_1"],
+                sentence_2=self.val_data["sentence_2"],
+                labels=self.val_data["label"],
                 tokenizer=self.tokenizer,
                 max_len=self.max_len,
             )
         elif stage == "predict":
-            self.test_dataset = TestDataset(
-                sentence_1=self.test_data["sentence_1"],
-                sentence_2=self.test_data["sentence_2"],
+            self.predict_dataset = TestDataset(
+                sentence_1=self.predict_data["sentence_1"],
+                sentence_2=self.predict_data["sentence_2"],
                 tokenizer=self.tokenizer,
                 max_len=self.max_len,
             )
@@ -52,7 +66,7 @@ class TextDataLoader(L.LightningDataModule):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
-        return DataLoader(self.dev_dataset, batch_size=self.batch_size)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
 
     def predict_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+        return DataLoader(self.predict_dataset, batch_size=self.batch_size)
