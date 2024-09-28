2c9c792afcdb95e5285d33a524f5b1f5b077ea68
KyungJun <kangjun205@gmail.com>
2024-09-28 20:23:24 +0900

    chore: Remove unused module

--- utils/util.py
+++ utils/util.py
@@ -6,18 +6,6 @@ import torch
 import wandb
 
 
-def model_load(run_name, model_path, project_name="Level1-STS"):
-    run = wandb.init(project=project_name, name=run_name)
-    config = run.config
-
-    artifact = wandb.use_artifact(f"{project_name}/{model_path}:latest")
-    model_dir = artifact.download(root="./saved")
-
-    model = torch.load(f"{model_dir}/model.pth")
-
-    return model, config
-
-
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
