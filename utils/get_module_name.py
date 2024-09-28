0b5389c6a42b1472bbfaf04a3967cc99a985ccae
KyungJun <kangjun205@gmail.com>
2024-09-28 20:14:19 +0900

    docs: Add doc string to get_module_name.py

--- utils/get_module_name.py
+++ utils/get_module_name.py
@@ -4,6 +4,15 @@ from transformers import AutoModel
 
 
 def main(arg):
+    """_summary_
+    LoRA 적용을 위한 module name print
+
+    특정 모델에 대해 LoRA 적용을 위해,
+    해당 모델을 로드하여 모델의 module name을 print
+    Args:
+        arg (argparse.Namespace): command line arguments
+            - model_name : module name을 검색할 model
+    """
     MODEL_NAME = arg.model_name
     model = AutoModel.from_pretrained(MODEL_NAME)
     for name, module in model.named_modules():
